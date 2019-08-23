19d96d6acae76d37ac629550e527f4c849edebab
MatthewTighe <matthewdtighe@gmail.com>
2019-08-23 14:01:04 -0700

    Add end-to-end test for entire implemented flow.

--- app/src/androidTest/java/tighe/matthew/expanserpgsheet/MainActivityTest.kt
+++ app/src/androidTest/java/tighe/matthew/expanserpgsheet/MainActivityTest.kt
@@ -7,13 +7,43 @@ import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import tighe.matthew.expanserpgsheet.model.CharacterModel
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
     @get:Rule val activityRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun test_MainActivity_hosts_nav_fragment() {
+    fun testMainActivityHostsNavFragment() {
         R.id.nav_host_fragment.isDisplayed()
+    }
+
+    @Test
+    fun testCharacterCanBeAddedThenModified() {
+        val expectedName = "name"
+        val expectedInitialFortune = 25
+        // Start fragment is character list
+        R.id.layout_fragment_character_list.isDisplayed()
+
+        // Nav and create new character
+        R.id.btn_create_new.click()
+        R.id.input_name.writeText(expectedName)
+        R.id.input_max_fortune.writeText(expectedInitialFortune.toString())
+        R.id.btn_save.click()
+
+        // Character has been added to list. Click it to nav to details.
+        R.id.layout_fragment_character_list.isDisplayed()
+        expectedName.isDisplayed()
+        expectedName.click()
+
+        // Character fortune can be modified
+        R.id.layout_fragment_character_details.isDisplayed()
+        expectedInitialFortune.toString().isDisplayed()
+        R.id.text_increment_five.click()
+        val incrementedFortune = expectedInitialFortune + 5
+        incrementedFortune.toString().isDisplayed()
+
+        R.id.text_decrement_five.click()
+        expectedInitialFortune.toString().isDisplayed()
     }
 }
