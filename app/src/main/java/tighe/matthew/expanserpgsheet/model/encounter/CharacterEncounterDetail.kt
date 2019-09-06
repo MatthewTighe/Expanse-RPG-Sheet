98aa694e11ec55d346d03ada5b6461b73299a3b8
MatthewTighe <matthewdtighe@gmail.com>
2019-09-06 12:27:06 -0700

    Codify relation between encounter details and characters.

--- app/src/main/java/tighe/matthew/expanserpgsheet/model/encounter/CharacterEncounterDetail.kt
+++ app/src/main/java/tighe/matthew/expanserpgsheet/model/encounter/CharacterEncounterDetail.kt
@@ -1,13 +1,21 @@
 package tighe.matthew.expanserpgsheet.model.encounter
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 import tighe.matthew.expanserpgsheet.model.Model
 import tighe.matthew.expanserpgsheet.model.character.Character
 
 @Parcelize
-@Entity(tableName = "character_encounter_detail")
+@Entity(tableName = "character_encounter_detail",
+    foreignKeys = [ForeignKey(
+        entity = Character::class,
+        parentColumns = arrayOf("id"),
+        childColumns = arrayOf("characterId"),
+        onDelete = ForeignKey.CASCADE
+    )]
+)
 data class CharacterEncounterDetail(
     @PrimaryKey val position: Int,
     val initiative: Int,
